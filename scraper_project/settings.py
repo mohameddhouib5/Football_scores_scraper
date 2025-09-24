4279ca1648d94e556759974545ea87340403798e
mohamed dhouib <mohameddhouib741@gmail.com>
2025-09-24 21:56:25 +0100

    third commit

--- scraper_project/settings.py
+++ scraper_project/settings.py
@@ -149,3 +149,13 @@ LOGGING = {
         },
     },
 }
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",  # 👈 add this line
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
